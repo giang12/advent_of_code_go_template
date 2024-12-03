342b1ee11a22063a2a4772e1dd8d8bfd70feb83d
zen <zen@x1111.xyz>
2024-12-02 19:34:48 -0500

    day 1

--- days/day_01/main.go
+++ days/day_01/main.go
@@ -2,6 +2,12 @@ package day_01
 
 import (
 	"fmt"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
+
+	"advent_of_code_go_template/util"
 )
 
 // Run function of the daily challenge
@@ -14,12 +20,46 @@ func Run(input []string, mode int) {
 	}
 }
 
+func parseInput(input []string) ([]int, []int) {
+	var lefts []int
+	var rights []int
+	for i := 0; i < len(input); i++ {
+		parts := strings.Fields(input[i])
+		left, _ := strconv.Atoi(parts[0])
+		right, _ := strconv.Atoi(parts[1])
+		lefts = append(lefts, left)
+		rights = append(rights, right)
+	}
+	return lefts, rights
+}
+
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	var lefts, rights []int = parseInput(input)
+
+	sort.Slice(lefts, func(i, j int) bool {
+		return lefts[i] < lefts[j]
+	})
+	sort.Slice(rights, func(i, j int) bool {
+		return rights[i] < rights[j]
+	})
+	var total int = 0
+
+	for i := 0; i < len(lefts); i++ {
+		total += int(math.Abs(float64(lefts[i] - rights[i])))
+	}
+	//fmt.Printf("leftSet: %v\n", leftSet)
+	//fmt.Printf("rightSet: %v\n", rightSet)
+	return strconv.Itoa(total)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	var lefts, rights []int = parseInput(input)
+	var freqs map[int]int = util.GetFrequencyMap(rights)
+	var ans int = 0
+	for _, v := range lefts {
+		ans += (v * freqs[v])
+	}
+	return strconv.Itoa(ans)
 }
