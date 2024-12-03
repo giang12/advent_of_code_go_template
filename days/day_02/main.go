5727c078164dde25c0a23a7b894eb016fe9bd56a
zen <zen@x1111.xyz>
2024-12-02 21:13:07 -0500

    day 2

--- days/day_02/main.go
+++ days/day_02/main.go
@@ -1,7 +1,10 @@
 package day_02
 
 import (
+	"advent_of_code_go_template/util"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 // Run function of the daily challenge
@@ -14,12 +17,60 @@ func Run(input []string, mode int) {
 	}
 }
 
+func isReportSafe(levels []string) (bool, int) {
+	var inc int = util.Atoi(levels[1]) - util.Atoi(levels[0])
+
+	var i int = 1
+	if inc == 0 {
+		return false, i
+	}
+
+	for ; i < len(levels); i++ {
+		var diff int = util.Atoi(levels[i]) - util.Atoi(levels[i-1])
+		var abs int = util.Abs(diff)
+
+		//bounds
+		if abs < 1 || abs > 3 {
+			return false, i
+		}
+		//different direction
+		if (diff < 0 && inc > 0) || (diff > 0 && inc < 0) {
+			return false, i
+		}
+	}
+	return true, -1
+}
+
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	var ans int = 0
+	for _, report := range input {
+		isSafe, _ := isReportSafe(strings.Fields(report))
+		if isSafe {
+			ans += 1
+		}
+	}
+	return strconv.Itoa(ans)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	var ans int = 0
+	for _, report := range input {
+		var cur []string = strings.Fields(report)
+		isSafe, _ := isReportSafe(cur)
+		if !isSafe {
+			for i := 0; i < len(cur); i++ {
+				test_report_input := util.RemoveIndex(cur, i)
+				isSafe, _ = isReportSafe(test_report_input)
+				if isSafe {
+					break
+				}
+			}
+		}
+		if isSafe {
+			ans += 1
+		}
+	}
+	return strconv.Itoa(ans)
 }
--- util/util.go
+++ util/util.go
@@ -1,9 +1,30 @@
 package util
 
+import (
+	"math"
+	"strconv"
+)
+
 func GetFrequencyMap(right []int) map[int]int {
 	m := make(map[int]int)
 	for _, v := range right {
 		m[v]++
 	}
 	return m
+}
+
+func Atoi(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
+
+func Abs(a int) int {
+	return int(math.Abs(float64(a)))
+}
+
+func RemoveIndex(cur []string, i int) []string {
+	newArr := make([]string, len(cur)-1)
+	copy(newArr, cur[:i])
+	copy(newArr[i:], cur[i+1:])
+	return newArr
 }
