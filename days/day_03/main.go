3158f925f21245036783994160c276596361bbf2
zen <zen@x1111.xyz>
2024-12-04 13:09:10 -0500

    day 3

--- days/day_03/main.go
+++ days/day_03/main.go
@@ -2,6 +2,8 @@ package day_03
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 )
 
 // Run function of the daily challenge
@@ -15,11 +17,51 @@ func Run(input []string, mode int) {
 }
 
 // Part1 solves the first part of the exercise
-func Part1(input []string) string {
-	return ""
+func Part1(inputs []string) string {
+	total := 0
+	for _, input := range inputs {
+		for _, instr := range getInstructions(input, `mul\(\d+,\d+\)`) {
+
+			total += mul(instr)
+		}
+	}
+	return strconv.Itoa(total)
 }
 
 // Part2 solves the second part of the exercise
-func Part2(input []string) string {
-	return ""
+func Part2(inputs []string) string {
+	total := 0
+	do := true
+	for _, input := range inputs {
+		for _, instr := range getInstructions(input, `mul\(\d+,\d+\)|do\(\)|don't\(\)`) {
+			if instr == "do()" {
+				do = true
+			} else if instr == "don't()" {
+				do = false
+			} else if do {
+				total += mul(instr)
+			}
+		}
+	}
+	return strconv.Itoa(total)
+}
+
+func getInstructions(input string, reg string) []string {
+	pattern := regexp.MustCompile(reg)
+	return pattern.FindAllString(input, -1)
+}
+func mul(mul string) int {
+	pattern := regexp.MustCompile(`\d+`)
+	var numbers []int
+	for _, s := range pattern.FindAllString(mul, -1) {
+		num, err := strconv.Atoi(s)
+
+		if err != nil {
+			fmt.Println("Error converting string to integer:", err)
+			continue
+		}
+
+		numbers = append(numbers, num)
+	}
+	return numbers[0] * numbers[1]
 }
